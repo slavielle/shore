41ad5a6085dd893237c58758a58717e688d2a2de
slavielle <sylvain.lavielle@netelios.fr>
2016-08-20 22:36:13 +0200

    adding variable in settings to optionally open brower window on container start

--- settings/settings.example.sh
+++ settings/settings.example.sh
@@ -2,3 +2,4 @@ CONTAINER_IP="172.18.0.2"
 CONTAINER_NETWORK="dockerhut_net"
 IMAGE_ID="9debb3555932"
 GIT_URL="https://github.com/slavielle/dockerhut_sample_workspace.git"
+OPEN_BROWSER_ON_START=true
