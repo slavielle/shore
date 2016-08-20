4fc3481434b578415c6de3e6c07b1305483b87af
slavielle <sylvain.lavielle@netelios.fr>
2016-08-21 00:35:02 +0200

    Add Mysql PW env var

--- settings/settings.example.sh
+++ settings/settings.example.sh
@@ -3,3 +3,4 @@ CONTAINER_NETWORK="dockerhut_net"
 IMAGE_ID="9debb3555932"
 GIT_URL="https://github.com/slavielle/dockerhut_sample_workspace.git"
 OPEN_BROWSER_ON_START=true
+MYSQL_ROOT_PW="my-sql-root-pw"
