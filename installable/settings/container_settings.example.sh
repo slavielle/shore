8fcd42bdd6adc72dcb8f7bd0e74f0dc8d8e0b8cd
slavielle <sylvain.lavielle@netelios.fr>
2016-08-25 13:49:04 +0200

    Setup hook script mysql_create_project_database_and_user

--- installable/settings/container_settings.example.sh
+++ installable/settings/container_settings.example.sh
@@ -2,4 +2,7 @@
 #PROJECT_HTDOCS="www"
 #PROJECT_DOMAIN_NAME="dockerhut-test.local"
 #MYSQL_ROOT_PASSWORD="RoOtPas5"
+MYSQL_PROJECT_USER_NAME="dockerhut_test"
+MYSQL_PROJECT_DB_NAME="dockerhut_test"
+MYSQL_PROJECT_USER_PASSWORD="dockerhut_tes7_pa5s"
 SERVICES_TO_START=("apache2" "mysql")
