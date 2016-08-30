f1789bfe92c388ca38b77255885fc19aa76280bd
slavielle <sylvain.lavielle@netelios.fr>
2016-08-30 21:38:03 +0200

    Prefix runtime variable with "RT_" + Hook script ruby_sass_install

--- delivery/settings.example.sh
+++ delivery/settings.example.sh
@@ -18,3 +18,10 @@ CONF_DRUSH_VERSION="8.1.3"
 
 CONF_NODE_VERSION="lts"
 
+# Default Ruby version will be the latest stable. If you want a specific ruby 
+# version, uncomment following line and set the ruby version as expected by the
+# command rvm instal 
+#CONF_RUBY_VERSION="ruby-1.9.3-p551"
+
+CONF_RUBY_GEMS=("bundler" "sass")
+
