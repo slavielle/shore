038386c6a5d12f12afa6137bee27d57697566438
slavielle <sylvain.lavielle@netelios.fr>
2016-09-06 21:16:09 +0200

    Comments

--- delivery/bundles/ruby/settings.example.sh
+++ delivery/bundles/ruby/settings.example.sh
@@ -1,15 +1,15 @@
 # Default Ruby version will be the latest stable. If you want a specific ruby 
 # version, uncomment following line and set the ruby version as expected by the
-# command rvm instal.
+# command "rvm install".
 # CONF_RUBY_VERSION="ruby-1.9.3-p551"
 
 # Set the directpry for installing gems. This path is a relative path from your 
-# project directory
+# project directory.
 # CONF_RUBY_GEM_INSTALL_PATH="path/to/gem/dir"
 
 # List of Gems to install.
 CONF_RUBY_GEMS=("bundler" "sass")
 
-# If your gem directory contains a Gemfile uncomment following variable to 
-# perform a bundle install 
+# If your gem directory contains a Gemfile, uncomment following variable to 
+# perform a "bundle install" (bundler).
 # CONF_RUBY_USE_BUNDLER=true
