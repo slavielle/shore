6b8aea24f0544ae9704d187167ce97beefc67868
slavielle <sylvain.lavielle@netelios.fr>
2016-09-06 21:12:40 +0200

    Add "npm install" to node bundle"s install procedure.

--- delivery/bundles/node/settings.example.sh
+++ delivery/bundles/node/settings.example.sh
@@ -1,2 +1,7 @@
-#node version to provide to the n command. See https://github.com/tj/n
+# Node version to provide to the n command. See https://github.com/tj/n .
 CONF_NODE_VERSION="lts"
+
+# Path where the package.json is located. This path is a relative path from your 
+# project directory.
+#CONF_NODE_PACKAGE_JSON_DIR="path/to/your/dir/containing/node/package/file"
+
