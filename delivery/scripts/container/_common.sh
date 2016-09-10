563ceff54072fb727238ce253e9dc77be54ca1c3
slavielle <sylvain.lavielle@netelios.fr>
2016-09-11 01:28:29 +0200

    Handling new "bundles" file.
    Handling new "bundles" file in profiles directory describing enabled bundles for the given profile.

--- delivery/scripts/container/_common.sh
+++ delivery/scripts/container/_common.sh
@@ -43,6 +43,19 @@ function include_files(){
 }
 
 
+array_contains () { 
+    local ARRAY="$1[@]"
+    local SEEKING=$2
+    local IN=1
+    for ELEMENT in "${!ARRAY}"; do
+        if [[ $ELEMENT == $SEEKING ]]; then
+            IN=0
+            break
+        fi
+    done
+    return $IN
+}
+
 
 function run_hook() {
 
@@ -55,7 +68,26 @@ function run_hook() {
     # Remap host path to container path
     ORIGIN_CONTAINER_DIR_PATH=${ORIGIN_HOST_DIR_PATH/$RT_HOST_SHARED_DIR_PATH/$CONTAINER_PROJECT_DIR_PATH}
 
+    ENABLED_BUNDLES=()
+    ENABLED_BUNDLES_LIST_FILE=$(readlink -f "$(dirname "$0")/../../profiles/$CONF_PROFILE/bundles")
+    while read BUNDLE || [[ -n "$BUNDLE" ]]; do
+
+        # Trim
+        if [[ "$BUNDLE" =~ ^[[:space:]]*([^[:space:]].*[^[:space:]])[[:space:]]*$ ]]; then 
+            BUNDLE=${BASH_REMATCH[1]}
+        fi
+
+        # Add bundle to ENABLED_BUNDLES array if bundle name match
+        if [[ $BUNDLE =~ ^([0-9a-z_]+)$ ]]; then
+            ENABLED_BUNDLES+=("$BUNDLE")
+        elif [[ $BUNDLE != \#* ]] && [ ! -z "$BUNDLE" ] ; then
+            echo ""; format_output " > MISMATCH : \"$BUNDLE\" in \"$ENABLED_BUNDLES_LIST_FILE\" " "error" "\n"
+        fi
+    done <"$ENABLED_BUNDLES_LIST_FILE"
+
+
     # Loop on script list for the given hook
+    HOOK_SCRIPT_LIST_FILE=$(readlink -f "$(dirname "$0")/../../profiles/$CONF_PROFILE/on_$HOOK_NAME.hook")
     while read HOOK_SCRIPT || [[ -n "$HOOK_SCRIPT" ]]; do
         
         # Trim
@@ -65,20 +97,23 @@ function run_hook() {
 
         # Test if $HOOK_SCRIPT match the regexp
         if [[ $HOOK_SCRIPT =~ ^([0-9a-z_]+)\.([0-9a-z_]*)$ ]]; then
-            
-            # Run the hook script and display result
-            BUNDLE="${BASH_REMATCH[1]}"
-            FNC="${BASH_REMATCH[2]}"
-            echo ""; format_output " > INVOKE $BUNDLE.$FNC " "hook" "\n\n"
-            "$(dirname "$0")/../../bundles/$BUNDLE/hookable_scripts/$FNC" $ORIGIN_CONTAINER_DIR_PATH $@
-
+            HOOK_BUNDLE=${BASH_REMATCH[1]}
+            if array_contains ENABLED_BUNDLES $HOOK_BUNDLE; then
+                # Run the hook script and display result
+                BUNDLE="${BASH_REMATCH[1]}"
+                FNC="${BASH_REMATCH[2]}"
+                echo ""; format_output " > INVOKE $BUNDLE.$FNC " "hook" "\n\n"
+                "$(dirname "$0")/../../bundles/$BUNDLE/hookable_scripts/$FNC" $ORIGIN_CONTAINER_DIR_PATH $@
+            else
+                echo ""; format_output " > BUNDLE DISABLED FOR: \"$HOOK_SCRIPT\" " "error" "\n"
+            fi
         elif [[ $HOOK_SCRIPT != \#* ]] && [ ! -z "$HOOK_SCRIPT" ] ; then
 
             # Catch unmatching values (blank lines and commented lines are ignored)
-            echo ""; format_output " > MISMATCH : \"$HOOK_SCRIPT\" " "error" "\n"
+            echo ""; format_output " > MISMATCH : \"$HOOK_SCRIPT\" in \"$HOOK_SCRIPT_LIST_FILE\" " "error" "\n"
         fi
 
-    done <"$(dirname "$0")/../../profiles/$CONF_PROFILE/on_$HOOK_NAME.hook"
+    done <"$HOOK_SCRIPT_LIST_FILE"
 }
 
 
