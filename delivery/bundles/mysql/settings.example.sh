8dd596e7200c18a33c8358caba2441f8058bfb80
slavielle <sylvain.lavielle@netelios.fr>
2016-09-20 09:19:39 +0200

    Comment

--- delivery/bundles/mysql/settings.example.sh
+++ delivery/bundles/mysql/settings.example.sh
@@ -1,4 +1,6 @@
 CONF_MYSQL_ROOT_PASSWORD="RoOtPas5"
+
+#Project database user name : Must be no longer than 16 char
 CONF_MYSQL_PROJECT_USER_NAME="shore_test"
 CONF_MYSQL_PROJECT_DB_NAME="shore_test"
 CONF_MYSQL_PROJECT_USER_PASSWORD="shore_tes7_pa5s"
