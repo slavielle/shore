f0e16d0e5a83a82c5b7df5af136dd4675ab130c1
slavielle <sylvain.lavielle@netelios.fr>
2016-09-26 14:59:07 +0200

    New symfony bundle

---
+++ delivery/bundles/symfony/settings.example.sh
@@ -0,0 +1,6 @@
+# Symfony root directory from your project directory.
+# If Symfony root directory  is you project directory, let this variable commented.
+#CONF_SYMFONY_ROOT_DIR="symfony"
+
+# List of directory on which apache have to write.
+CONF_SYMFONY_APACHE_USER_DIRECTORY_LIST=( "app/logs" "app/cache" )
