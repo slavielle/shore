466eb911efb351294ebcba287ae0ecd08965e838
slavielle <sylvain.lavielle@netelios.fr>
2016-10-01 00:33:26 +0200

    Add settings in node bundle settings file

--- delivery/bundles/node/settings.example.sh
+++ delivery/bundles/node/settings.example.sh
@@ -1,6 +1,9 @@
 # Node version to provide to the n command. See https://github.com/tj/n .
 CONF_NODE_VERSION="lts"
 
+# List of node module to install globally.
+# CONF_NODE_INSTALL_GLOBAL=( "less" )
+
 # Path where the package.json is located. This path is a relative path from your 
 # project directory.
 #CONF_NODE_PACKAGE_JSON_DIR="path/to/your/dir/containing/node/package/file"
