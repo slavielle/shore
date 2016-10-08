8688eea72ca2d08ac58608b26400324a937a46b2
slavielle <sylvain.lavielle@netelios.fr>
2016-10-09 00:50:36 +0200

    PHP5 bundle - define php lib to install as settings var

---
+++ delivery/bundles/php5/settings.example.sh
@@ -0,0 +1,10 @@
+CONF_PHP5_LIBS=()
+
+# Activates or add php libs required by your project.
+CONF_PHP5_LIBS+=("php-pear")
+CONF_PHP5_LIBS+=("php5-mysql")
+CONF_PHP5_LIBS+=("php5-gd")
+# CONF_PHP5_LIBS+=("php5-curl")
+# CONF_PHP5_LIBS+=("php5-xsl")
+
+
