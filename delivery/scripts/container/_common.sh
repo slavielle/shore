02627daca4c575e7f2f8d1c55c609903481ab34c
slavielle <sylvain.lavielle@netelios.fr>
2017-03-08 08:37:15 +0100

    New global hooks to avoid duplicate some hooks in every profiles

--- delivery/scripts/container/_common.sh
+++ delivery/scripts/container/_common.sh
@@ -18,6 +18,10 @@ function format_output() {
       CODE_IN="\e[41m"
         ;;
 
+    error_item)
+      CODE_IN="\e[91m"
+        ;;
+
     warning)
       CODE_IN="\e[93m"
         ;;
@@ -87,45 +91,70 @@ function run_hook() {
     done <"$ENABLED_BUNDLES_LIST_FILE"
 
 
-    # Loop on script list for the given hook
+    # Locate the hook.
     HOOK_SCRIPT_LIST_FILE=$(realpath "$(dirname "$0")/../../profiles/$RT_PROFILE/on_$HOOK_NAME.hook")
-    while read HOOK_SCRIPT || [[ -n "$HOOK_SCRIPT" ]]; do
-        
-        # Trim
-        if [[ "$HOOK_SCRIPT" =~ ^[[:space:]]*([^[:space:]].*[^[:space:]])[[:space:]]*$ ]]; then 
-            HOOK_SCRIPT=${BASH_REMATCH[1]}
+    if [ ! -f "$HOOK_SCRIPT_LIST_FILE" ]; then
+        HOOK_SCRIPT_LIST_FILE=$(realpath "$(dirname "$0")/../../hooks/on_$HOOK_NAME.hook")
+    fi
+
+    # Loop on script list for the given hook
+    MODE_LIST=("check" "run")
+    ERRORS=0
+    for MODE in ${MODE_LIST[@]}; do
+        if (( $ERRORS == 0 )) ; then
+            LINE_COUNT=0
+            while read HOOK_SCRIPT || [[ -n "$HOOK_SCRIPT" ]]; do
+                LINE_COUNT=$((LINE_COUNT + 1))
+                # Trim
+                if [[ "$HOOK_SCRIPT" =~ ^[[:space:]]*([^[:space:]].*[^[:space:]])[[:space:]]*$ ]]; then 
+                    HOOK_SCRIPT=${BASH_REMATCH[1]}
+                fi
+
+                # Test if $HOOK_SCRIPT match the regexp
+                if [[ $HOOK_SCRIPT =~ ^([0-9a-z_]+)\.([0-9a-z_]*)$ ]]; then
+
+                    HOOK_BUNDLE=${BASH_REMATCH[1]}
+
+                    if array_contains ENABLED_BUNDLES $HOOK_BUNDLE; then
+
+                        if [ "$MODE" == "run" ]; then
+
+                            # Get the name of the bundle and action to run
+                            BUNDLE="${BASH_REMATCH[1]}"
+                            FNC="${BASH_REMATCH[2]}"
+
+                            # Define the log file part 2 name
+                            echo "RT_LOG_FILES_NAME_PART_2=\"-$BUNDLE-$FNC\"" > "$CONTAINER_PROJECT_DIR_PATH/.shore/runtime/RT_LOG_FILES_NAME_PART_2.var.sh"
+
+                            # Run the hook script and display result
+                            echo ""; format_output " > INVOKE $BUNDLE.$FNC " "hook" "\n\n"
+                            "$(dirname "$0")/../../bundles/$BUNDLE/hookable_scripts/$FNC" $ORIGIN_CONTAINER_DIR_PATH $@
+                        fi
+
+                    else
+
+                        if [ "$MODE" == "check" ]; then
+                            ERRORS=$((ERRORS + 1))
+                            echo ""; format_output " > Error(L$LINE_COUNT): \"$HOOK_BUNDLE\" bundle disabled on \"$HOOK_SCRIPT\" " "error_item"
+                        fi
+                    fi
+
+                elif [[ $HOOK_SCRIPT != \#* ]] && [ ! -z "$HOOK_SCRIPT" ] ; then
+
+                    if [ "$MODE" == "check" ]; then
+                        # Catch unmatching values (blank lines and commented lines are ignored)
+                        ERRORS=$((ERRORS + 1))
+                        echo ""; format_output " > Error(L$LINE_COUNT): Line mismatch on \"$HOOK_SCRIPT\"" "error_item"
+                    fi
+                fi
+
+            done <"$HOOK_SCRIPT_LIST_FILE"
         fi
+    done
 
-        # Test if $HOOK_SCRIPT match the regexp
-        if [[ $HOOK_SCRIPT =~ ^([0-9a-z_]+)\.([0-9a-z_]*)$ ]]; then
-
-            HOOK_BUNDLE=${BASH_REMATCH[1]}
-
-            if array_contains ENABLED_BUNDLES $HOOK_BUNDLE; then
-
-                # Get the name of the bundle and action to run
-                BUNDLE="${BASH_REMATCH[1]}"
-                FNC="${BASH_REMATCH[2]}"
-
-                # Define the log file part 2 name
-                echo "RT_LOG_FILES_NAME_PART_2=\"-$BUNDLE-$FNC\"" > "$CONTAINER_PROJECT_DIR_PATH/.shore/runtime/RT_LOG_FILES_NAME_PART_2.var.sh"
-
-                # Run the hook script and display result
-                echo ""; format_output " > INVOKE $BUNDLE.$FNC " "hook" "\n\n"
-                "$(dirname "$0")/../../bundles/$BUNDLE/hookable_scripts/$FNC" $ORIGIN_CONTAINER_DIR_PATH $@
-
-            else
-
-                echo ""; format_output " > BUNDLE DISABLED FOR: \"$HOOK_SCRIPT\" " "error" "\n"
-
-            fi
-        elif [[ $HOOK_SCRIPT != \#* ]] && [ ! -z "$HOOK_SCRIPT" ] ; then
-
-            # Catch unmatching values (blank lines and commented lines are ignored)
-            echo ""; format_output " > MISMATCH : \"$HOOK_SCRIPT\" in \"$HOOK_SCRIPT_LIST_FILE\" " "error" "\n"
-        fi
-
-    done <"$HOOK_SCRIPT_LIST_FILE"
+    if (( $ERRORS > 0 )) ; then
+        echo "";echo ""; format_output " > ABORT : Errors detected on hook \"$HOOK_NAME\" in \"$HOOK_SCRIPT_LIST_FILE\"" "error" "\n";echo ""
+    fi
 }
 
 
