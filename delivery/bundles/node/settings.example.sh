78a916ad2621d3c54ba6a34fb0b0c555e5deb03a
slavielle <sylvain.lavielle@netelios.fr>
2017-03-24 23:21:37 +0100

    Bundle node - debug global node modules installation

--- delivery/bundles/node/settings.example.sh
+++ delivery/bundles/node/settings.example.sh
@@ -1,8 +1,10 @@
 # Node version to provide to the n command. See https://github.com/tj/n .
 CONF_NODE_VERSION="lts"
 
-# List of node module to install globally.
-# CONF_NODE_INSTALL_GLOBAL=( "less" )
+# Activates(uncomment) or add node modules to install globally for your project.
+CONF_NODE_INSTALL_GLOBAL=()
+CONF_NODE_INSTALL_GLOBAL+=("gulp-cli")
+CONF_NODE_INSTALL_GLOBAL+=("bower")
 
 # Path to where the package.json is located. This path is a relative path from  
 # your project directory.
