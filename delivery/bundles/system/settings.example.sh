83cfef9f656e8f177ce11257727e31f6f1479893
slavielle <sylvain.lavielle@netelios.fr>
2017-03-24 23:06:05 +0100

    System bundle - add install script

---
+++ delivery/bundles/system/settings.example.sh
@@ -0,0 +1,9 @@
+CONF_SYSTEM_INSTALL_LIST=()
+
+# Activates(uncomment) or add system elements required by your project.
+CONF_SYSTEM_INSTALL_LIST+=("curl")
+CONF_SYSTEM_INSTALL_LIST+=("rsync")
+CONF_SYSTEM_INSTALL_LIST+=("openssh-client")
+# CONF_SYSTEM_INSTALL_LIST+=("vim")
+
+
