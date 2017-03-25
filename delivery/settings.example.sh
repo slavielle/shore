155ef7609c7360ca747a3232a2a7a3f26366c8d4
slavielle <sylvain.lavielle@netelios.fr>
2017-03-25 01:52:09 +0100

    Add comments to main settings file.

--- delivery/settings.example.sh
+++ delivery/settings.example.sh
@@ -1,17 +1,45 @@
-CONF_CONTAINER_IP="172.18.0.2"
-CONF_HOST_IP="172.18.0.1"
+# Docker bridge name.
 CONF_CONTAINER_NETWORK="shore_net"
+
+# Container IP in the Docker bridge range.
+# Its advised to define a new one for each of your local projets. This way, you
+# would be able to run them together.
+CONF_CONTAINER_IP="172.18.0.2"
+
+# Host ip in the Docker bridge network.
+# Change it carefully.
+CONF_HOST_IP="172.18.0.1"
+
+# Docker source image to use.
+# Change this is not advised. Change it carefully.
+# Mandatory
 CONF_IMAGE_ID="debian:jessie"
+
+# If set to true, Shore will attempt to open browser at start.
 CONF_OPEN_BROWSER_ON_START=true
+
+# Services to start on the container.
 CONF_SERVICES_TO_START=("apache2" "mysql")
 
+# project name. 
+# Mandatory.
 CONF_PROJECT_NAME="shore_test"
-#CONF_PROJECT_DOMAIN_NAME="shore-test.local"
+
+# Define your project local domain name.
+# Not mandatory.
+# CONF_PROJECT_DOMAIN_NAME="shore-test.local"
+
+# Webserver entry point. 
+# Mandatory.
 CONF_PROJECT_HTDOCS="www"
+
+# Webserver entry point script.
+# Not Mandatory.
 #CONF_PROJECT_INDEX_FILE="index.php"
+
+# Change this is not advised. Change it carefully.
+# Mandatory.
 CONF_PROJECT_CONTAINER_SIDE_PATH="/var/shore"
-
-
 
 # Ports mapping list.
 # Shore does not require to map ports on Linux because Shore use static IP to 
