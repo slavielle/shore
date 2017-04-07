7d1b6862f5a633fd153854fe6cc93f4aeaaaa3db
slavielle <sylvain.lavielle@netelios.fr>
2017-04-07 22:37:21 +0200

    Add sudo to system bundle

--- delivery/bundles/system/settings.example.sh
+++ delivery/bundles/system/settings.example.sh
@@ -1,6 +1,7 @@
 CONF_SYSTEM_INSTALL_LIST=()
 
 # Activates(uncomment) or add system elements required by your project.
+CONF_SYSTEM_INSTALL_LIST+=("sudo")
 CONF_SYSTEM_INSTALL_LIST+=("curl")
 CONF_SYSTEM_INSTALL_LIST+=("rsync")
 CONF_SYSTEM_INSTALL_LIST+=("openssh-client")
