e657b07f76cc6c9ecb4d1382059bc468bc0421ee
slavielle <sylvain.lavielle@netelios.fr>
2018-06-04 12:51:38 +0200

    Add minimal requirements to system bundle

--- delivery/bundles/system/settings.example.sh
+++ delivery/bundles/system/settings.example.sh
@@ -3,8 +3,13 @@ CONF_SYSTEM_INSTALL_LIST=()
 # Activates(uncomment) or add system elements required by your project.
 CONF_SYSTEM_INSTALL_LIST+=("sudo")
 CONF_SYSTEM_INSTALL_LIST+=("curl")
+CONF_SYSTEM_INSTALL_LIST+=("wget")
 CONF_SYSTEM_INSTALL_LIST+=("rsync")
 CONF_SYSTEM_INSTALL_LIST+=("openssh-client")
+CONF_SYSTEM_INSTALL_LIST+=("apt-transport-https")
+CONF_SYSTEM_INSTALL_LIST+=("lsb-release")
+CONF_SYSTEM_INSTALL_LIST+=("ca-certificates")
+
 # CONF_SYSTEM_INSTALL_LIST+=("vim")
 
 
