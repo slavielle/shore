0913635dc78d26303b8d9febb59c86a572b753df
slavielle <sylvain.lavielle@netelios.fr>
2018-09-17 23:37:13 +0200

    Add Drush 9 support

--- delivery/bundles/drush/settings.example.sh
+++ delivery/bundles/drush/settings.example.sh
@@ -1,1 +1,3 @@
-CONF_DRUSH_VERSION="8.1.3"
+# Keep only one of the two variables uncommented in order to install Drush or drush launcher (for drush > 9) globally
+#CONF_DRUSH_VERSION="8.1.3"
+CONF_DRUSH_LAUNCHER_VERSION="0.6.0"
