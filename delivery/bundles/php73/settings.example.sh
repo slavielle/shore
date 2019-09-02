9edb2606d52d91baac29072bfc58d9f40539a86a
slavielle <sylvain.lavielle@netelios.fr>
2019-09-02 18:56:56 +0200

    Add PHP7.3 bundle

---
+++ delivery/bundles/php73/settings.example.sh
@@ -0,0 +1,16 @@
+CONF_PHP71_LIBS=()
+
+# Activates or add php libs required by your project.
+CONF_PHP71_LIBS+=("php7.3-opcache")
+CONF_PHP71_LIBS+=("libapache2-mod-php7.3")
+CONF_PHP71_LIBS+=("php7.3-mysql")
+CONF_PHP71_LIBS+=("php7.3-curl")
+CONF_PHP71_LIBS+=("php7.3-json")
+CONF_PHP71_LIBS+=("php7.3-gd")
+CONF_PHP71_LIBS+=("php7.3-mcrypt")
+CONF_PHP71_LIBS+=("php7.3-intl")
+CONF_PHP71_LIBS+=("php7.3-mbstring")
+CONF_PHP71_LIBS+=("php7.3-xml")
+CONF_PHP71_LIBS+=("php7.3-zip")
+CONF_PHP71_LIBS+=("php7.3-fpm")
+CONF_PHP71_LIBS+=("php7.3-readline")
